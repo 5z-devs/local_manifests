b25294730572c06130ebd3d105d42825a08be33e
Marc Bourgoin <themard85@gmail.com>
2024-04-27 22:05:29 -0600

    Update for QPR2

--- Z01R_clean_build.sh
+++ Z01R_clean_build.sh
@@ -16,8 +16,9 @@ source build/envsetup.sh
 
 # clean the out dir; comment out, if you want to do
 # a dirty build
+breakfast lineage_Z01R-ap1a-userdebug
 make -j9 ARCH=arm clean
 
 # fire up the building process and also log stdout
 # and stderrout
-brunch lineage_Z01R-userdebug 2>&1 | tee Z01R_make.log
+brunch lineage_Z01R-ap1a-userdebug 2>&1 | tee Z01R_make.log
--- Z01R_dirty_build.sh
+++ Z01R_dirty_build.sh
@@ -16,9 +16,9 @@ source build/envsetup.sh
 
 # clean the out dir; comment out, if you want to do
 # a dirty build
-# make -j9 ARCH=arm clean
+breakfast lineage_Z01R-ap1a-userdebug
 make installclean
 
 # fire up the building process and also log stdout
 # and stderrout
-brunch lineage_Z01R-userdebug 2>&1 | tee Z01R_make.log
+brunch lineage_Z01R-ap1a-userdebug 2>&1 | tee Z01R_make.log
